9da98dfda5744ed3d443c193965482654d17ed44
Qiu Yingmu <1832599386@qq.com>
2026-05-21 11:24:07 +0800

    Add -Dfile.encoding=UTF-8 JVM arg

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -2,4 +2,4 @@
 # Start nginx
 nginx -g 'daemon off;' &
 # Start backend
-java -jar /app/app.jar --spring.profiles.active=prod
+java -Dfile.encoding=UTF-8 -jar /app/app.jar --spring.profiles.active=prod
